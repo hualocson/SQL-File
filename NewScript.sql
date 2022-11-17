1200255b41cd53a6066c988955567c3a759b513d
htien120802 <dhtien120802@gmail.com>
2022-11-17 14:09:53 +0700

    new update

--- NewScript.sql
+++ NewScript.sql
@@ -6,3 +6,18 @@ CREATE OR ALTER PROC getTeamLeadInCompany(@id int)
 AS (SELECT Member.id, Member.name FROM Member, role WHERE is_deleted = 0 AND Member.role = role.id AND role.name = 'Team Lead' AND team_id is NULL AND company_id = @id)
 GO
 -- End update
+
+-- Update 17/11/2022 2:03 PM
+CREATE OR ALTER FUNCTION getMemberByUsername (@username varchar(50), @password varchar(50)) RETURNS int
+AS 
+	BEGIN
+		DECLARE @value int
+		SELECT @value = id FROM Member WHERE is_deleted = 0 AND username = @username AND password = @password
+		IF (@value is NULL)
+			BEGIN
+				SET @value = 0
+			END
+		RETURN @value
+	END
+GO
+-- End update
